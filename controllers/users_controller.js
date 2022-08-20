3ec9a56ef8f8c975deb11abbad5e0726126aa029
akshatk806 <akshatk806@gmail.com>
2022-08-20 22:30:20 +0530

    4: Adding routers and controllers to sign-up page and setting view of sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,9 @@ module.exports.signin=function(request,response){
         title:"GGSIPU | Sign in"
     })
 }
+
+module.exports.signup=function(request,response){
+    return response.render('sign_up',{
+        title:"GGSIPU | Sign up"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const users_controller=require('../controllers/users_controller.js')
 const router=express.Router();
 
 router.get('/sign-in',users_controller.signin)
+router.get('/sign-up',users_controller.signup)
 
 module.exports=router
